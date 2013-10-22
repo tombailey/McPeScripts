b9bbdffea5fa7cebe3bdc655a56d22df3162411d
tombailey <inbox@tombailey.me>
2013-10-22 21:51:48 +0100

    Update
    World height for McPe maps is 128 not 256; code corrected.

--- TreeChopper_tombailey94.js
+++ TreeChopper_tombailey94.js
@@ -1,5 +1,5 @@
-// TreeChopper mod by tombailey94
-// version 0.0.1
+// TreeChopper mod by tombailey94; with credit to Donalddoesmc for correcting me on the world height (http://www.minecraftforum.net/user/2365623-donalddoesmc/)
+// version 0.0.2
 
 var axeIds = [258, 271, 275, 279, 286];
 var woodId = 17;
@@ -11,11 +11,11 @@ function useItem(x,y,z,itemId,blockId,side) {
 			var tile = getTile(x, y, z); 
 			Level.dropItem(x, y, z, 0, tile, 1, Level.getData()); //drops wood on the ground
 			Level.destroyBlock(x, y, z, true); //destroys wood block
-			for(var i = y;i < 256;i++) { //loop to look for wood blocks above original block
-				tile = getTile(x,i,z);
+			for(var i = y;i < 128;i++) { //loop to look for wood blocks above original block
+				tile = getTile(x, i, z);
 				if(tile == woodId) { //if the block is a wood block
-					Level.dropItem(x,y,z,0,tile,1,Level.getData()); //drops wood on the ground
-					Level.destroyBlock(x,i,z,false); //destroys wood block
+					Level.dropItem(x, y, z, 0, tile, 1, Level.getData()); //drops wood on the ground
+					Level.destroyBlock(x, i, z, false); //destroys wood block
 				}
 			}
 		}
