1e1a7c70e4aa9faa0f3f1ceebd23db84ea51e27d
tombailey <inbox@tombailey.me>
2013-11-05 10:25:21 +0000

    Update
    Corrected my logic for the TnT Explode script, thanks to PEMapModder.

--- MagicWand_tombailey94.js
+++ MagicWand_tombailey94.js
@@ -1,5 +1,5 @@
 // MagicWand mod by tombailey94; with credit to PEMapModder for correcting my silly unused variable and optional parameter mistakes (https://github.com/pemapmodder)
-// version 0.0.1
+// version 0.0.2
 
 var quantity = 64; //the amount of blocks to give the player
 var wandId = 280; //the data value for "the wand" (280 is sticks)
--- TnTExplode.js
+++ TnTExplode.js
@@ -1,5 +1,5 @@
-// TnT Explode mod by tombailey94
-// version 0.0.1
+// TnT Explode mod by tombailey94; with credit to PEMapModder for correcting my silly 'or' instead of 'and' conditional logic (https://github.com/pemapmodder)
+// version 0.0.2
 var wandId = 50; //the data value for "the wand" (50 is torches)
 var tntDataValue = 46; //current block id (0.7.6) for TnT
 var explosionRadius = 2;
