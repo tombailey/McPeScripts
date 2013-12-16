68800a2d8a33a89c93a6eb8ea2d74987bdbb911c
tombailey <inbox@tombailey.me>
2013-12-16 21:37:21 +0000

    Updated Achievements

--- Achievements_tombailey94.js
+++ Achievements_tombailey94.js
@@ -1,8 +1,8 @@
 //Achievements mod by tombailey94
 // version 0.0.1
 
-var achvs = [false,false,false,false,false];
-var achvNames = ["Getting Wood","Bedtime already?","Into The Nether","DIAMONDS!","A beacon of hope"];
+var achvs = [false,false,false,false];
+var achvNames = ["Getting Wood","Bedtime already?","Into The Nether","DIAMONDS!"];
 
 function unlockAchv(intIndex){
 	achvs[intIndex] = true;
@@ -19,32 +19,30 @@ function destroyBlock(x, y, z, side) {
 }
 
 function useItem(x,y,z,itemId,blockId,side) {
-	if(blockId == 247 && !achvs[2]) { //if player has touched a nether reactor core for the frist time
+	if(blockId == 247 && !achvs[2]) { //if player has touched a nether reactor core for the first time
 		unlockAchv(2);
-	} else if(blockId == 26 && !achvs[1]) { //if player has touched a bed for the frist time
+	} else if(blockId == 26 && !achvs[1]) { //if player has touched a bed for the first time
 		unlockAchv(1);
-	} else if(blockId == 50 && !achvs[4]) { //if player has place a torch for the frist time
-		unlockAchv(4);
 	}
 }
 
 function newLevel() { //load world
-	for(var i=0;i!=achvs.length-1;i++){
+	for(var i=0;i<achvs.length;i++){
 		achvs[i] = ModPE.readData("achievement" + i);
 	}
 }
 
 function leaveGame() { //exit world
-	for(var i=0;i!=achvs.length-1;i++){
+	for(var i=0;i<achvs.length;i++){
 		ModPE.saveData("achievement" + i, achvs[i]);
 	}
 }
 
 function procCmd(cmd) {
 	var commandElements = cmd.split(" ");
-	if(commandElements[0] == "achievements") { //if command was /achievements // suggestion: shorten it
+	if(commandElements[0] == "achvs") { //if command was /achievements // suggestion: shorten it
 		if(commandElements.length==1) {
-			for(var i=0;i!=achvs.length-1;i++){
+			for(var i=0;i<achvs.length;i++){
 				if(achvs[i]) { //achievement unlocked
 					clientMessage(achvNames[i] + " unlocked.");
 				} else { //achievement locked
@@ -53,14 +51,14 @@ function procCmd(cmd) {
 			}
 		} else if(commandElements.length==2){
 			if(commandElements[1] == "reset") {
-				for(var i=0;i!=achvs.length-1;i++){
+				for(var i=0;i<achvs.length;i++){
 					achvs[i] == false;
 				}
 			} else {
-				clientMessage("Your parameters were wrong. Achievements command only allows for 'reset' paramter.");
+				clientMessage("Your parameters were wrong. Achievements command only allows for 'reset' parameter.");
 			}
 		} else {
-			clientMessage("Your parameters were wrong. Achievements command only allows for 'reset' paramter.");
+			clientMessage("Your parameters were wrong. Achievements command only allows for 'reset' parameter.");
 		}
 	}
 }
