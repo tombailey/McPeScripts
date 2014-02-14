f25e85d9bac03fce94223f7654dd442573571d16
tombailey <inbox@tombailey.me>
2014-02-14 21:42:50 +0000

    Removed two scripts
    DayAndNight script removed as it is included in BasicCommands.
    TnTExplode script removed as 0.8.x and above allow flint and steel in
    creative for setting TnT off.

--- DayAndNight.js
+++
@@ -1,9 +0,0 @@
-// Day and Night mod by tombailey94
-// version 0.0.1
-function procCmd(cmd) {
-	if(cmd == "day") { //if command was /day
-		Level.setTime(0); //change time to day
-	} else if (cmd == "night") { //if command was /night
-		Level.setTime(14000); //change time to night
-	}
-}
