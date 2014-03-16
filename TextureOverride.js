c4b9b30a981531c2d5ad0d18d072276a429b99e6
tombailey <inbox@tombailey.me>
2014-03-16 20:55:25 +0000

    New tester script
    TextureOverride changes the default texture for Gold to a LuckyBlock

---
+++ TextureOverride.js
@@ -0,0 +1,3 @@
+function newLevel() {
+	ModPE.overrideTexture("images/terrain-atlas.tga","http://www.tombailey.me/terrain-atlas.tga"); //turns gold into LuckyBlocks (image made by me) by replacing terrain-atlas.tga file with a custom one (permanent unless reinstall)
+}
