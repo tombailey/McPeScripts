dd345359e115055f54f4bfb12499e0bb6e4a985d
tombailey <inbox@tombailey.me>
2014-03-27 18:32:52 +0000

    TextureOverride example update
    Uses "/textures on" and "/textures off" as an example of how to turn
    textures on and off

--- TextureOverride_tombailey94.js
+++ TextureOverride_tombailey94.js
@@ -1,3 +1,12 @@
-function newLevel() {
-	ModPE.overrideTexture("images/terrain-atlas.tga","http://www.tombailey.me/terrain-atlas.tga"); //turns gold into LuckyBlocks (image made by me) by replacing terrain-atlas.tga file with a custom one (permanent unless reinstall)
+function procCmd(cmd) {
+	var command = cmd.split(" ");
+	if(command[0] == "textures" && command.length == 2) {
+		if (command[1] == "on") {
+			ModPE.overrideTexture("images/terrain-atlas.tga","http://www.tombailey.me/projects/modpe/luckyblocks.tga");
+			clientMessage("Restart BlockLauncher, with an active internet connection, to apply this.");
+		} else if (command[1] == "off") {
+			ModPE.resetImages();
+			clientMessage("Restart BlockLauncher to apply this.");
+		}
+	}
 }
