e2cc7f31b0e15fe94a25704d40b734a08125513f
tombailey <inbox@tombailey.me>
2014-04-02 00:30:52 +0100

    LuckyBlock update
    A few tweaks; mainly trying to make things more random and less
    frequent.

--- LuckyBlock_tombailey94.js
+++ LuckyBlock_tombailey94.js
@@ -1,5 +1,5 @@
 //LuckyBlock mod by tombailey94; with credit to PlayerInDistress (http://www.minecraftforum.net/topic/2031111-164-lucky-block-drops-items-spawns-mobs-structures-and-more/) and NOAHZ (http://www.minecraftforum.net/topic/2174428-noahzdarkdiaminers-modscripts-new-darkdiamondminers-mods/)
-// version 0.0.2
+// version 0.0.3
 
 var goldId = 41;
 
@@ -24,7 +24,8 @@ function destroyBlock(x, y, z, side) {
 		
 		if (rnd == 0 || rnd == 1) {
 			rnd = Math.floor(Math.random()*(5)); //random number between 0 and 4
-			Level.setTime(12000); //set to night for mobs not to burn
+			
+			Level.setTime(8280); //set to night for mobs not to burn (doesn't seem to work :S)
 			
 			if (rnd == 0) { //creepers
 				Level.spawnMob(x+4,y,z+4,33);
@@ -65,31 +66,39 @@ function destroyBlock(x, y, z, side) {
 				Level.spawnMob(x-4,y,z-4,35);
 			}			
 		} else if (rnd == 2 || rnd == 3 || rnd == 4 || rnd == 5 || rnd == 6 || rnd == 7 || rnd == 8) {
-			rnd = Math.floor(Math.random()*(10)); //random number between 0 and 10
+			rnd = Math.floor(Math.random()*(10)); //random number between 0 and 9
 			
-			if (rnd == 0 || rnd == 1) { //primed tnt
-				Level.spawnMob(x+1,y,z,65);
-				Level.spawnMob(x,y,z+1,65);
-			} else if (rnd == 2 || rnd == 3) { //compass, clock, bow and arrows 
+			if (rnd == 0) { //diamond armour
+				Level.dropItem(x,y,z,0,310,1,0); 
+				Level.dropItem(x,y,z,0,311,1,0);
+				Level.dropItem(x,y,z,0,312,1,0);
+				Level.dropItem(x,y,z,0,313,64,0);
+			} else if (rnd == 1) { //compass, clock, bow and arrows 
 				Level.dropItem(x,y,z,0,345,1,0); 
 				Level.dropItem(x,y,z,0,347,1,0);
 				Level.dropItem(x,y,z,0,261,1,0);
 				Level.dropItem(x,y,z,0,262,64,0);
-			} else if (rnd == 4 || rnd == 5) { //leather armour
+			} else if (rnd == 2) { //leather armour
 				Level.dropItem(x,y,z,0,298,1,0); 
 				Level.dropItem(x,y,z,0,299,1,0);
 				Level.dropItem(x,y,z,0,300,1,0);
 				Level.dropItem(x,y,z,0,301,1,0);
-			} else if (rnd == 6) { //drops a iron sword, shovel and pickaxe
+			} else if (rnd == 3) { //drops a iron sword, shovel and pickaxe
 				Level.dropItem(x,y,z,0,267,1,0); 
 				Level.dropItem(x,y,z,0,256,1,0);
 				Level.dropItem(x,y,z,0,257,1,0);
-			} else if (rnd == 7) { //drops a stone sword, hoe and axe
+			} else if (rnd == 4) { //drops a stone sword, hoe and axe
 				Level.dropItem(x,y,z,0,272,1,0); 
 				Level.dropItem(x,y,z,0,291,1,0);
 				Level.dropItem(x,y,z,0,275,1,0);
-			} else if (rnd == 8 | rnd == 9) { //carrots
+			} else if (rnd == 5) { //carrots
 				Level.dropItem(x,y,z,0,391,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 carrots
+			} else if (rnd == 6) { //apples
+				Level.dropItem(x,y,z,0,260,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 apples
+			} else if (rnd == 7) { //emeralds
+				Level.dropItem(x,y,z,0,388,Math.floor(Math.random()*(10)+1),0); //drop anywhere from 1 to 10 emeralds
+			} else if (rnd == 8) { //gunpowder
+				Level.dropItem(x,y,z,0,289,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 gunpowder
 			} else { //luck stuff
 				Level.dropItem(x,y,z,0,264,Math.floor(Math.random()*(3)+1),0); //drop anywhere from 1 to 3 diamonds
 				Level.dropItem(x,y,z,0,265,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 iron
@@ -97,7 +106,7 @@ function destroyBlock(x, y, z, side) {
 			}
 			
 		} else {
-			rnd = Math.floor(Math.random()*(2)); //random number between 0 and 1
+			rnd = Math.floor(Math.random()*(3)); //random number between 0 and 2
 			
 			var playerX = getPlayerX();
 			var playerY = getPlayerY();
@@ -128,7 +137,7 @@ function destroyBlock(x, y, z, side) {
 				
 				clientMessage("Look up!");
 			
-			} else { //sandstone structure
+			} else if (rnd == 1) { //sandstone structure
 				setTile(playerX+2, playerY, playerZ-2, 24);
 				setTile(playerX-2, playerY, playerZ+2, 24);
 				setTile(playerX-2, playerY, playerZ-2, 24);
@@ -184,6 +193,9 @@ function destroyBlock(x, y, z, side) {
 				setTile(playerX+1, playerY-1, playerZ+1, goldId);
 				
 				
+			} else { //primed TnT
+				Level.spawnMob(x+1,y,z,65);
+				Level.spawnMob(x,y,z+1,65);
 			}
 		}
 		
