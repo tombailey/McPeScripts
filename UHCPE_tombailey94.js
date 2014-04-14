00b92653d4f15c825904311a711b72e23da1b7f2
tombailey <inbox@tombailey.me>
2014-04-14 20:20:23 +0100

    New Script Ultra Hard Core Pocket Edition
    New Script, Ultra Hard Core Pocket Edition makes Minecraft PE more
    difficult. At the moment, 1 in 3 block drops disappear and mobs have
    double health. More to come :D

---
+++ UHCPE_tombailey94.js
@@ -0,0 +1,18 @@
+// Ultra Hard Core Pocket Edition by tombailey94
+// version 0.0.1
+
+function procCmd(cmd) {
+	
+}
+
+function entityAddedHook(entity) {
+	var id = Entity.getEntityTypeId(entity);
+	if (id == 64) { 
+		var rnd = Math.floor((Math.random()*3)+1);
+		if (rnd == 1) { //1 in every 3 blocks dropped gets removed
+			Entity.remove(entity);
+		}
+	} else {
+		Entity.setHealth(entity,Entity.getHealth(entity)*2); //double health
+	}	
+}
