db30e9808aa2f8ca3bb79a53e6da741d8bceeb83
tombailey <inbox@tombailey.me>
2014-11-15 15:01:32 +0000

    Added 0.9.5 mobs and updated render types
    Render type constants according to
    https://github.com/Connor4898/ModPE-Scripts/wiki/Render-types

--- MobDisguise_tombailey94.js
+++ MobDisguise_tombailey94.js
@@ -6,43 +6,59 @@ function procCmd(command) {
 	command = command.split(" ");
 	switch (command[0].toLowerCase()) {
 		case "chicken":
-			Entity.setRenderType(Player.getEntity(), 6);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.chicken);
 			Entity.setMobSkin(Player.getEntity(),"mob/chicken.png");
 			break;
 		case "cow":
-			Entity.setRenderType(Player.getEntity(), 7);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.cow);
 			Entity.setMobSkin(Player.getEntity(),"mob/cow.png");
 			break;
 		case "creeper":
-			Entity.setRenderType(Player.getEntity(), 14);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.creeper);
 			Entity.setMobSkin(Player.getEntity(),"mob/creeper.png");
 			break;
+		case "enderman":
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.enderman);
+			Entity.setMobSkin(Player.getEntity(),"mob/enderman.png");
+			break;
+		case "mooshroomcow":
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.mushroomCow);
+			Entity.setMobSkin(Player.getEntity(),"mob/mooshroom.png");
+			break;
 		case "pig":
-			Entity.setRenderType(Player.getEntity(), 8);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.pig);
 			Entity.setMobSkin(Player.getEntity(),"mob/pig.png");
 			break;
 		case "sheep":
-			Entity.setRenderType(Player.getEntity(), 9);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.sheep);
 			Entity.setMobSkin(Player.getEntity(),"mob/sheep_0.png");
 			break;
+		case "silverfish":
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.silverfish);
+			Entity.setMobSkin(Player.getEntity(),"mob/silverfish.png");
+			break;
 		case "skeleton":
-			Entity.setRenderType(Player.getEntity(), 12);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.skeleton);
 			Entity.setMobSkin(Player.getEntity(),"mob/skeleton.png");
 			break;
 		case "spider":
-			Entity.setRenderType(Player.getEntity(), 13);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.spider);
 			Entity.setMobSkin(Player.getEntity(),"mob/spider.png");
 			break;
 		case "steve":
-			Entity.setRenderType(Player.getEntity(), 3);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.player);
 			Entity.setMobSkin(Player.getEntity(),"mob/char.png");
 			break;
+		case "villager":
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.player);
+			Entity.setMobSkin(Player.getEntity(),"mob/villager/villager.png");
+			break;
 		case "zombie":
-			Entity.setRenderType(Player.getEntity(), 11);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.player);
 			Entity.setMobSkin(Player.getEntity(),"mob/zombie.png");
 			break;
 		case "zombiepigman":
-			Entity.setRenderType(Player.getEntity(), 11);
+			Entity.setRenderType(Player.getEntity(), EntityRenderType.player);
 			Entity.setMobSkin(Player.getEntity(),"mob/pigzombie.png");
 			break;
 	}
