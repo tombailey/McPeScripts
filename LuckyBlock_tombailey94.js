f7e2534e6959953765c5a782976bdd5ec415d617
tombailey <inbox@tombailey.me>
2015-01-29 22:44:18 +0000

    Updated script
    General tidy up.
    
    Added and tweaked a few things. Hopefully Lucky Blocks is now more
    random.

--- LuckyBlock_tombailey94.js
+++ LuckyBlock_tombailey94.js
@@ -1,194 +1,155 @@
 //LuckyBlock mod by tombailey94; with credit to PlayerInDistress (http://www.minecraftforum.net/topic/2031111-164-lucky-block-drops-items-spawns-mobs-structures-and-more/) and NOAHZ (http://www.minecraftforum.net/topic/2174428-noahzdarkdiaminers-modscripts-new-darkdiamondminers-mods/)
-// version 0.0.4
+// version 0.0.5
 
 var goldId = 41;
 
-ModPE.overrideTexture("images/terrain-atlas.tga","http://www.tombailey.me/projects/modpe/luckyblocks.tga");
-
-function procCmd(cmd) {
-	if(cmd == "resettextures") {
-		ModPE.resetImages();
-		clientMessage("Restart BlockLauncher to apply this.");
-	}
-}
-
 function destroyBlock(x, y, z, side) {
-	var blockId = getTile(x,y,z);
+	var blockId = Level.getTile(x, y, z);
 	
 	if (blockId == goldId) {
-		var rnd = Math.floor(Math.random()*(10)); //(pseudo) random number between 0 and 9
-		
+		var rnd = random(0, 9);
+
 		if (rnd == 0 || rnd == 1) {
-			rnd = Math.floor(Math.random()*(5)); //random number between 0 and 4
+			rnd = random(0, 4);
 			
-			Level.setTime(8280); //set to night for mobs not to burn (doesn't seem to work :S)
+			//Level.setTime(8280); //set to night for mobs not to burn (doesn't seem to work :S)
+			
+			var mobs = [];
 			
 			if (rnd == 0) { //creepers
-				Level.spawnMob(x+4,y,z+4,33);
-				Level.spawnMob(x-4,y,z+4,33);
-				Level.spawnMob(x+4,y,z-4,33);
-				Level.spawnMob(x-4,y,z-4,33);
+				mobs.push(33);
 			} else if (rnd == 1) { //zombie pigmen
-				Level.spawnMob(x+4,y,z+4,32);
-				Level.spawnMob(x-4,y,z+4,32);
-				Level.spawnMob(x+4,y,z-4,32);
-				Level.spawnMob(x-4,y,z-4,32);
+				mobs.push(32);
 			} else if (rnd == 2) { //skeletons
-				Level.spawnMob(x+4,y,z+4,34);
-				Level.spawnMob(x-4,y,z+4,34);
-				Level.spawnMob(x+4,y,z-4,34);
-				Level.spawnMob(x-4,y,z-4,34);
+				mobs.push(34);
 			} else if (rnd == 3) { //spiders
-				Level.spawnMob(x+4,y,z+4,35);
-				Level.spawnMob(x-4,y,z+4,35);
-				Level.spawnMob(x+4,y,z-4,35);
-				Level.spawnMob(x-4,y,z-4,35);
+				mobs.push(35);
 			} else { //all of the above
-				Level.spawnMob(x+4,y,z+4,33);
-				Level.spawnMob(x-4,y,z+4,33);
-				Level.spawnMob(x+4,y,z-4,33);
-				Level.spawnMob(x-4,y,z-4,33);
-				Level.spawnMob(x+4,y,z+4,32);
-				Level.spawnMob(x-4,y,z+4,32);
-				Level.spawnMob(x+4,y,z-4,32);
-				Level.spawnMob(x-4,y,z-4,32);
-				Level.spawnMob(x+4,y,z+4,34);
-				Level.spawnMob(x-4,y,z+4,34);
-				Level.spawnMob(x+4,y,z-4,34);
-				Level.spawnMob(x-4,y,z-4,34);
-				Level.spawnMob(x+4,y,z+4,35);
-				Level.spawnMob(x-4,y,z+4,35);
-				Level.spawnMob(x+4,y,z-4,35);
-				Level.spawnMob(x-4,y,z-4,35);
-			}			
-		} else if (rnd == 2 || rnd == 3 || rnd == 4 || rnd == 5 || rnd == 6 || rnd == 7 || rnd == 8) {
-			rnd = Math.floor(Math.random()*(10)); //random number between 0 and 9
-			
-			if (rnd == 0) { //diamond armour
-				Level.dropItem(x,y,z,0,310,1,0); 
-				Level.dropItem(x,y,z,0,311,1,0);
-				Level.dropItem(x,y,z,0,312,1,0);
-				Level.dropItem(x,y,z,0,313,64,0);
-			} else if (rnd == 1) { //compass, clock, bow and arrows 
-				Level.dropItem(x,y,z,0,345,1,0); 
-				Level.dropItem(x,y,z,0,347,1,0);
-				Level.dropItem(x,y,z,0,261,1,0);
-				Level.dropItem(x,y,z,0,262,64,0);
-			} else if (rnd == 2) { //leather armour
-				Level.dropItem(x,y,z,0,298,1,0); 
-				Level.dropItem(x,y,z,0,299,1,0);
-				Level.dropItem(x,y,z,0,300,1,0);
-				Level.dropItem(x,y,z,0,301,1,0);
-			} else if (rnd == 3) { //drops a iron sword, shovel and pickaxe
-				Level.dropItem(x,y,z,0,267,1,0); 
-				Level.dropItem(x,y,z,0,256,1,0);
-				Level.dropItem(x,y,z,0,257,1,0);
-			} else if (rnd == 4) { //drops a stone sword, hoe and axe
-				Level.dropItem(x,y,z,0,272,1,0); 
-				Level.dropItem(x,y,z,0,291,1,0);
-				Level.dropItem(x,y,z,0,275,1,0);
-			} else if (rnd == 5) { //carrots
-				Level.dropItem(x,y,z,0,391,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 carrots
-			} else if (rnd == 6) { //apples
-				Level.dropItem(x,y,z,0,260,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 apples
-			} else if (rnd == 7) { //emeralds
-				Level.dropItem(x,y,z,0,388,Math.floor(Math.random()*(10)+1),0); //drop anywhere from 1 to 10 emeralds
-			} else if (rnd == 8) { //gunpowder
-				Level.dropItem(x,y,z,0,289,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 gunpowder
-			} else { //luck stuff
-				Level.dropItem(x,y,z,0,264,Math.floor(Math.random()*(3)+1),0); //drop anywhere from 1 to 3 diamonds
-				Level.dropItem(x,y,z,0,265,Math.floor(Math.random()*(6)+1),0); //drop anywhere from 1 to 6 iron
-				Level.dropItem(x,y,z,0,266,Math.floor(Math.random()*(3)+1),0); //drop anywhere from 1 to 3 diamonds
+				mobs.push(33);
+				mobs.push(32);
+				mobs.push(34);
+				mobs.push(35);
+			}
+
+			for (var i in mobs) {
+				for (var j = 0; j < 4; j++) {
+					var xIncrement = (j % 2 == 0 ? 4 : -4);
+					var zIncrement = ((j == 0 || j == 1) ? 4 : -4);
+					
+					Level.spawnMob(x+xIncrement, y, z+zIncrement, mobs[i]);
+				}
 			}
 			
+		} else if (rnd > 1 && rnd < 9) {
+			rnd = random(0, 12);
+			
+			if (rnd == 0) { //compass and clock
+				Level.dropItem(x,y,z,0,345,1,0); 
+				Level.dropItem(x,y,z,0,347,1,0);
+			} else if (rnd == 1) { //bow and arrows 
+				Level.dropItem(x,y,z,0,261,1,0);
+				Level.dropItem(x,y,z,0,262,64,0);
+			} else if (rnd == 2) { //leather armour (cap and tunic)
+				Level.dropItem(x,y,z,0,298,1,0); 
+				Level.dropItem(x,y,z,0,299,1,0);
+			} else if (rnd == 3) { //leather armour (pants and boots)
+				Level.dropItem(x,y,z,0,300,1,0);
+				Level.dropItem(x,y,z,0,301,1,0);
+			} else if (rnd == 4) { //iron sword
+				Level.dropItem(x,y,z,0,267,1,0); 
+			} else if (rnd == 5) { //stone sword
+				Level.dropItem(x,y,z,0,272,1,0); 
+			} else if (rnd == 6) { //stone axe
+				Level.dropItem(x,y,z,0,275,1,0);
+			} else if (rnd == 7) { //carrots
+				Level.dropItem(x,y,z,0,391,random(1,6),0); //drop anywhere from 1 to 4 carrots
+			} else if (rnd == 6) { //apples
+				Level.dropItem(x,y,z,0,260,random(1,6),0); //drop anywhere from 1 to 3 apples
+			} else if (rnd == 7) { //emeralds
+				Level.dropItem(x,y,z,0,388,random(1,3),0); //drop anywhere from 1 to 3 emeralds
+			} else if (rnd == 8) { //diamond armour (helmet and leggings)
+				Level.dropItem(x,y,z,0,310,1,0);
+				Level.dropItem(x,y,z,0,312,1,0);
+			} else if (rnd == 9) { //diamond armour (chestplate and boots)
+				Level.dropItem(x,y,z,0,311,1,0);
+				Level.dropItem(x,y,z,0,313,1,0);
+			} else if (rnd == 10) { //lava bucket
+				Level.dropItem(x,y,z,0,327,1,0);
+			} else if (rnd == 11) { //mushroom stew
+				Level.dropItem(x,y,z,0,282,1,0);
+			} else { //diamonds and iron
+				Level.dropItem(x,y,z,0,264,random(1,3),0); //drop anywhere from 1 to 3 diamonds
+				Level.dropItem(x,y,z,0,265,random(1,5),0); //drop anywhere from 1 to 6 iron
+			}
+
 		} else {
-			rnd = Math.floor(Math.random()*(3)); //random number between 0 and 2
+			rnd = random(0,3); //random number between 0 and 2
 			
-			var playerX = getPlayerX();
-			var playerY = getPlayerY();
-			var playerZ = getPlayerZ();
+			var playerX = Player.getX();
+			var playerY = Player.getY();
+			var playerZ = Player.getZ();
 			
-			if (rnd == 0) { //creates a jail type structure (with iron bars) and spawns falling spawn above the players head
+			if (rnd == 0) { //creates a jail type structure (with iron bars) and spawns falling sand above the player's head
 				
-				setTile(playerX+1, playerY-1, playerZ, 101); //front, back and sides
-				setTile(playerX+1, playerY, playerZ, 101);
-				setTile(playerX-1, playerY-1, playerZ, 101);
-				setTile(playerX-1, playerY, playerZ, 101);
-				setTile(playerX, playerY-1, playerZ+1, 101); 
-				setTile(playerX, playerY-1, playerZ-1, 101);
-				setTile(playerX, playerY, playerZ+1, 101);
-				setTile(playerX, playerY, playerZ-1, 101);
+				var xs = [playerX-1, playerX, playerX+1];
+				var zs = [playerZ-1, playerZ, playerZ+1];
+
+				for (var i = 0; i < xs.length; i++) {
+					for (var j = 0; j < zs.length; j++) {
+						if (i != Math.floor(xs.length/2) || j != Math.floor(zs.length/2)) { //don't do the centre block
+							Level.setTile(xs[i], playerY, zs[j], 101); //iron bars
+							Level.setTile(xs[i], playerY-1, zs[j], 101);
+						}
+
+						Level.setTile(xs[i], playerY-2, zs[j], 98); //stone floor
+					}
+				}
 				
-				setTile(playerX+1, playerY-1, playerZ+1, 101); //corners
-				setTile(playerX+1, playerY, playerZ+1, 101);
-				setTile(playerX-1, playerY-1, playerZ+1, 101); 
-				setTile(playerX-1, playerY, playerZ+1, 101);
-				setTile(playerX+1, playerY-1, playerZ-1, 101); 
-				setTile(playerX+1, playerY, playerZ-1, 101);
-				setTile(playerX-1, playerY-1, playerZ-1, 101); 
-				setTile(playerX-1, playerY, playerZ-1, 101);
-				
-				setTile(playerX, playerY+50, playerZ, 12); //dat sand doe
-				setTile(playerX, playerY+51, playerZ, 12);
+				Level.setTile(playerX, playerY+2, playerZ, 11); //dat lava doe
 				
 				clientMessage("Look up!");
 			
 			} else if (rnd == 1) { //sandstone structure
-				setTile(playerX+2, playerY, playerZ-2, 24);
-				setTile(playerX-2, playerY, playerZ+2, 24);
-				setTile(playerX-2, playerY, playerZ-2, 24);
-				setTile(playerX+2, playerY, playerZ+2, 24);
-				setTile(playerX+2, playerY, playerZ, 24);
-				setTile(playerX-2, playerY, playerZ, 24);
-				setTile(playerX, playerY, playerZ+2, 24);
-				setTile(playerX, playerY, playerZ-2, 24);
+
+				var xs = [playerX-2, playerX-1, playerX, playerX+1, playerX+2];
+				var zs = [playerZ-2, playerZ-1, playerZ, playerZ+1, playerZ+2];
+
+				for (var i = 0; i < xs.length; i++) {
+					for (var j = 0; j < zs.length; j++) {
+						if ((i != 1 && j != 1 && j != 2 && j != 3) || (i != 2 && j != 1 && j != 2 && j != 3) || (i != 3 && j != 1 && j != 2 && j != 3)) { //don't do the 3x3 centre blocks
+							Level.setTile(xs[i], playerY, zs[j], 24);
+							Level.setTile(xs[i], playerY-1, zs[j], 24);
+						}
+
+						Level.setTile(xs[i], playerY-2, zs[j], 24);
+					}
+				}				
 				
-				setTile(playerX+2, playerY-1, playerZ-2, 24);
-				setTile(playerX-2, playerY-1, playerZ+2, 24);
-				setTile(playerX-2, playerY-1, playerZ-2, 24);
-				setTile(playerX+2, playerY-1, playerZ+2, 24);
-				setTile(playerX+2, playerY-1, playerZ, 24);
-				setTile(playerX-2, playerY-1, playerZ, 24);
-				setTile(playerX, playerY-1, playerZ+2, 24);
-				setTile(playerX, playerY-1, playerZ-2, 24);
-				
-				setTile(playerX+2, playerY, playerZ-1, 24);
-				setTile(playerX-2, playerY, playerZ+1, 24);
-				setTile(playerX-1, playerY, playerZ-2, 24);
-				setTile(playerX+1, playerY, playerZ+2, 24);
-				setTile(playerX+1, playerY, playerZ-2, 24);
-				setTile(playerX-1, playerY, playerZ+2, 24);
-				setTile(playerX-2, playerY, playerZ-1, 24);
-				setTile(playerX+2, playerY, playerZ+1, 24);
-				
-				setTile(playerX+2, playerY-1, playerZ-1, 24);
-				setTile(playerX-2, playerY-1, playerZ+1, 24);
-				setTile(playerX-1, playerY-1, playerZ-2, 24);
-				setTile(playerX+1, playerY-1, playerZ+2, 24);
-				setTile(playerX+1, playerY-1, playerZ-2, 24);
-				setTile(playerX-1, playerY-1, playerZ+2, 24);
-				setTile(playerX-2, playerY-1, playerZ-1, 24);
-				setTile(playerX+2, playerY-1, playerZ+1, 24);
-				
-				setTile(playerX, playerY-2, playerZ, 24); //under player
-				
-				setTile(playerX+1, playerY-2, playerZ-1, 24); //corners
-				setTile(playerX-1, playerY-2, playerZ+1, 24);
-				setTile(playerX-1, playerY-2, playerZ-1, 24);
-				setTile(playerX+1, playerY-2, playerZ+1, 24);
-				
-				setTile(playerX, playerY-2, playerZ-1, 24); //sides
-				setTile(playerX, playerY-2, playerZ+1, 24);
-				setTile(playerX-1, playerY-2, playerZ, 24);
-				setTile(playerX+1, playerY-2, playerZ, 24);
-				
-				
-				setTile(playerX+1, playerY-1, playerZ-1, goldId);
-				setTile(playerX-1, playerY-1, playerZ+1, goldId);
-				setTile(playerX-1, playerY-1, playerZ-1, goldId);
-				setTile(playerX+1, playerY-1, playerZ+1, goldId);
-				
-				
+				Level.setTile(playerX+1, playerY-1, playerZ-1, goldId);
+				Level.setTile(playerX-1, playerY-1, playerZ+1, goldId);
+				Level.setTile(playerX-1, playerY-1, playerZ-1, goldId);
+				Level.setTile(playerX+1, playerY-1, playerZ+1, goldId);
+
+			} else if (rnd == 2) { //obsidian structure
+
+				var xs = [playerX-1, playerX, playerX+1];
+				var zs = [playerZ-1, playerZ, playerZ+1];
+
+				for (var i = 0; i < xs.length; i++) {
+					for (var j = 0; j < zs.length; j++) {
+						if (i == Math.floor(xs.length/2) && j == Math.floor(zs.length/2)) { //water for the centre blocks
+							Level.setTile(xs[i], playerY, zs[j], 9);
+							Level.setTile(xs[i], playerY-1, zs[j], 9);
+						} else {
+							Level.setTile(xs[i], playerY, zs[j], 20); //glass on eye level
+							Level.setTile(xs[i], playerY-1, zs[j], 49); //obsidian below eye level
+						}
+
+						Level.setTile(xs[i], playerY+1, zs[j], 49); //obsidian roof
+						Level.setTile(xs[i], playerY-2, zs[j], 49); //obsidian floor
+					}
+				}
+
 			} else { //primed TnT
 				Level.spawnMob(x+1,y,z,65);
 				Level.spawnMob(x,y,z+1,65);
@@ -197,4 +158,8 @@ function destroyBlock(x, y, z, side) {
 		
 		Level.destroyBlock(x,y,z,false); //destroy the gold block
 	}
+}
+
+function random(min, max) { //inclusive
+	return Math.floor(Math.random()*(max+1)+min);
 }
