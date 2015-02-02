74c652443803bebfab69b3f38a32d0eb6bb1c30c
Samueljh1 <samueljh2001@gmail.com>
2015-02-02 19:56:11 +0000

    Update ExplosiveArrows_tombailey94.js
    Fixed Crashing and Simplified! :)

--- ExplosiveArrows_tombailey94.js
+++ ExplosiveArrows_tombailey94.js
@@ -1,34 +1,28 @@
-// ExplosiveArrows by tombailey94
+// ExplosiveArrows by tombailey94 & Samueljh1
 // version 0.0.1
 
-var Entities = [];
-var counter = 0;
+ent = null;
 
 function entityAddedHook(entity) {
 	if (Entity.getEntityTypeId(entity) == 80) { //arrow
-		var oldXYZ = [Entity.getX(entity),Entity.getY(entity),Entity.getZ(entity)];
-		Entities.push([entity,oldXYZ]);
+		ent = entity;
+	}
+}
+
+function entityRemovedHook(entity) {
+	if (Entity.getEntityTypeId(entity) == 80) { //arrow
+		ent = null;
 	}
 }
 
 function modTick() {
-	counter++;
 	
-	if(counter == 2) { //executed 10 times a second
-		for (i = 0; i < Entities.length; i++) {
+	if(ent != null){
+	
+		if(Entity.getVelX(ent) == 0 && Entity.getVelY(ent) == 0 && Entity.getVelZ(ent) == 0){
 		
-			var element = Entities[i];
-			var entity = element[0];
-			var oldXYZ = element[1];
-			var newXYZ = [Entity.getX(entity),Entity.getY(entity),Entity.getZ(entity)];
-			
-			if (oldXYZ[0] == newXYZ[0] && oldXYZ[1] == newXYZ[1] && oldXYZ[2] == newXYZ[2]) { //arrow hasn't moved
-				Entities.splice(i,1); //removes the entity as it no longer needs to be tracked
-				Level.explode(Entity.getX(entity),Entity.getY(entity),Entity.getZ(entity),5);
-			} else { //arrow has moved; update position
-				Entities[i][1] = newXYZ; //oldXYZ becomes the new XYZ
-			}
+			Level.explode(Entity.getX(ent),Entity.getY(ent),Entity.getZ(ent),5);
 		}
-		counter = 0;
+		
 	}
 }
