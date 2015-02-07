b3b201271c0a652385708087b8254fc4c9c14c8a
Adrian Webster <Adrian.webster57a@gmail.com>
2015-02-07 16:51:16 -0600

    Update SwitchGamemode_tombailey94.js

--- SwitchGamemode_tombailey94.js
+++ SwitchGamemode_tombailey94.js
@@ -1,9 +1,17 @@
-// Switch Gamemode mod by tombailey94
-// version 0.0.1
+/*
+Switch Gamemode mod by tombailey94
+version 0.0.2
+Checks gamemode (adrianwebster/adrianmodr)
+*/
+
 function procCmd(cmd) {
 	if(cmd == "survival") { //if command was /survival
+	if(Level.getGameMode()!=0){ //if gamemode wasnt survival
 		Level.setGameMode(0); //change to survival
+	}
 	} else if (cmd == "creative") { //if command was /creative
+	if(Level.getGameMode()!=1){ //if wasnt already creative
 		Level.setGameMode(1); //change to creative
+	}
 	}
 }
