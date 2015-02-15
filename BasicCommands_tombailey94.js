4edad2344ed7f4be0c009f206d6878cbf35e3a8e
Tom Bailey <tombailey1@hotmail.co.uk>
2015-02-15 15:42:14 +0000

    Updated script
    Query operator which looks better for SwitchGameMode and BasicCommands

--- BasicCommands_tombailey94.js
+++ BasicCommands_tombailey94.js
@@ -1,26 +1,18 @@
 // Basic Commands mod by tombailey94; uses the DayAndNight as well as SwtichGamemode mod also made by myself :)
-// version 0.0.1
+// version 0.0.2
+
 function procCmd(cmd) {
-	//commands without parameters. e.g. '/day'
-	//DayAndNight commands
-	if(cmd == "day") { //if command was /day
-		Level.setTime(0); //change time to day
+	if(cmd == "day") { 
+		Level.setTime(0); 
 		clientMessage("Time set to day.");
-	} else if (cmd == "night") { //if command was /night
-		Level.setTime(14000); //change time to night
+	} else if (cmd == "night") { 
+		Level.setTime(14000); 
 		clientMessage("Time set to night.");
-	} 
-	//SwitchGamemode commands
-	else if(cmd == "survival") { //if command was /survival
-		Level.setGameMode(0); //change to survival
-		clientMessage("Gamemode set to survival.");
-	} else if (cmd == "creative") { //if command was /creative
-		Level.setGameMode(1); //change to creative
-		clientMessage("Gamemode set to creative.");
-	}
-	//heal command
-	else if(cmd == "heal") { //if command was /heal
-		Player.setHealth(20); //heal the player; 20 half hearts or 10 whole hearts
+	} else if(cmd == "survival" || cmd == "creative") { 
+		Level.setGameMode(Level.getGameMode() ? 0 : 1);
+		clientMessage("Gamemode set to " + (Level.getGameMode() ? "survival" : "creative") + ".");
+	} else if(cmd == "heal") { 
+		Player.setHealth(20); 
 		clientMessage("Health set to 10 hearts.");
 	}
 }
--- SwitchGamemode_tombailey94.js
+++ SwitchGamemode_tombailey94.js
@@ -1,9 +1,9 @@
 // Switch Gamemode mod by tombailey94
-// version 0.0.1
+// version 0.0.2
+
 function procCmd(cmd) {
-	if(cmd == "survival") { //if command was /survival
-		Level.setGameMode(0); //change to survival
-	} else if (cmd == "creative") { //if command was /creative
-		Level.setGameMode(1); //change to creative
+	if(cmd == "survival" || cmd == "creative") { 
+		Level.setGameMode(Level.getGameMode() ? 0 : 1);
+		clientMessage("Gamemode set to " + (Level.getGameMode() ? "survival" : "creative") + ".");
 	}
 }
